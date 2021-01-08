c598872696d508b2fe52c7c50d90edc7e84a5fa3
Matheus Ferreira <matheusfs97@gmail.com>
2021-01-08 15:14:33 -0300

    hotfix: disable updated_at timestamp in Search model

--- src/Models/User/Search.php
+++ src/Models/User/Search.php
@@ -5,6 +5,8 @@ namespace MatheusFS\Laravel\Insights\Models\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Search extends Model {
+
+    const UPDATED_AT = null;
     
     protected $table = 'user_searches';
 
